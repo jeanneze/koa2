87a3312f2ac742a57dbe2f37cfc2b84dfa0cebeb
zhaojing834 <zhaojing834@jd.com>
2020-09-02 13:32:27 +0800

    ejs渲染

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,22 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    isMe: true,
+    blogList: [
+      {
+        id: 1,
+        title: '一场秋雨一场寒'
+      },
+      {
+        id: 2,
+        title: '天冷别忘加衣裳'
+      },
+      {
+        id: 3,
+        title: '愿你冬天有衣，夏日有雨'
+      }
+    ]
   })
 })
 
@@ -13,6 +28,23 @@ router.get('/string', async (ctx, next) => {
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async(ctx, next) => {
+  const { userName } = ctx.params   // ctx.params 可以获取到路由中所有参数
+  ctx.body = {
+    title: 'this is profile page',
+    userName
+  }
+})
+
+router.get('/profile/:userName/:pageIndex', async(ctx, next) => {
+  const { userName, pageIndex } = ctx.params   // ctx.params 可以获取到路由中所有参数
+  ctx.body = {
+    title: 'this is loadMore API',
+    userName,
+    pageIndex
   }
 })
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,12 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.post('/login', async (ctx, next) => {
+  const { userName, password } = ctx.request.body  // 可以获取post请求body中所有参数
+  ctx.body = {
+    userName,
+    password
+  }
+})
+
 module.exports = router
